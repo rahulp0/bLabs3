bdaed5e00888acc6448473aa0e0906a9fb3d762b
rahulp0 <rahul.prksh1@gmail.com>
2018-12-25 11:36:45 +0530

    except webpack.config.js

---
+++ app/main.js
@@ -0,0 +1,5 @@
+console.log('..');
+let app = (name) => {
+    console.log(`hello from ${name}`);
+}
+app('Rahul');
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//webpack.config.js
+
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        filename: 'bundle.js'
+    }/*,
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        port: 8080
+    }*/
+}
+
